5412e01205441652f57f685d907f5a1c99b43d33
Austen Adler <stonewareslord@gmail.com>
2015-01-10 11:46:30 -0500

    Collector now works

--- src/classes/Collector.cpp
+++ src/classes/Collector.cpp
@@ -1,22 +1,22 @@
 #include "Collector.h"
 DentCollector::DentCollector(int fl, int fr, int rl, int rr){
-  frontLeft = new Talon(fl);
-  frontRight = new Talon(fr);
+  collectorLeft = new Talon(fl);
+  collectorRight = new Talon(fr);
   raiserLeft = new Talon(rl);
   raiserRight = new Talon(rr);
 }
-void DentCollector::Collect(int power){
-  frontLeft->Set(power);
-  frontRight->Set(power);
+void DentCollector::Collect(float power){
+  collectorLeft->Set(power);
+  collectorRight->Set(power);
 }
-void DentCollector::Raise(int power){
+void DentCollector::Raise(float power){
   raiserLeft->Set(power);
   raiserRight->Set(power);
 }
 void DentCollector::Rest(){
   raiserLeft->Set(0);
   raiserRight->Set(0);
-  frontLeft->Set(0);
-  frontRight->Set(0);
+  collectorLeft->Set(0);
+  collectorRight->Set(0);
 }
 // vim: ts=2:sw=2:et
