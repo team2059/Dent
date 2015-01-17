0036f2dd7a2885bd0b56edcf6a446480b1f71b7d
Austen Adler <stonewareslord@gmail.com>
2015-01-16 19:49:16 -0500

    Added DentRobot.h

--- src/DentRobot.cpp
+++ src/DentRobot.cpp
@@ -1,38 +1,25 @@
-#include "WPILib.h"
-#include "Commands/Drive.h"
-#include "Commands/Collect.h"
-#include "Commands/Eject.h"
-#include "Commands/Raise.h"
-#include "Commands/Lower.h"
-class DentRobot: public IterativeRobot {
-private:
-  Command *autonomousCommand = NULL;
-  LiveWindow *lw;
-  void RobotInit() {
-    CommandBase::init();
-    lw = LiveWindow::GetInstance();
-  }
-  void DisabledPeriodic() {
-    Scheduler::GetInstance()->Run();
-  }
-  void AutonomousInit() {
-    if(autonomousCommand != NULL) {
-      autonomousCommand->Start();
-    }
-  }
-  void AutonomousPeriodic() {
-    Scheduler::GetInstance()->Run();
-  }
-  void TeleopInit() {
-    if(autonomousCommand != NULL) {
-      autonomousCommand->Cancel();
-    }
-  }
-  void TeleopPeriodic() {
-    Scheduler::GetInstance()->Run();
-  }
-  void TestPeriodic() {
-    lw->Run();
-  }
-};
+#include "DentRobot.h"
+DentRobot::DentRobot():
+  driveCommand(new Drive()){
+  printf("Initialized");
+}
+void DentRobot::RobotInit(){
+  printf("Initializing");
+  CommandBase::init();
+}
+void DentRobot::DisabledPeriodic(){
+  Scheduler::GetInstance()->Run();
+}
+void DentRobot::AutonomousInit(){
+}
+void DentRobot::AutonomousPeriodic(){
+  Scheduler::GetInstance()->Run();
+}
+void DentRobot::TeleopInit(){
+}
+void DentRobot::TeleopPeriodic(){
+  Scheduler::GetInstance()->Run();
+}
+void DentRobot::TestPeriodic(){
+}
 START_ROBOT_CLASS(DentRobot);
