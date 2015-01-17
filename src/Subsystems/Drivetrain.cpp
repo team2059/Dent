d9a0e30b6e746b4b9d322b1803059516c93b8709
Austen Adler <stonewareslord@gmail.com>
2015-01-17 10:34:08 -0500

    Added drivetrain code

--- src/Subsystems/Drivetrain.cpp
+++ src/Subsystems/Drivetrain.cpp
@@ -2,6 +2,17 @@
 #include "../RobotMap.h"
 
 Drivetrain::Drivetrain() : Subsystem("Drivetrain") {
+  frontLeft=new Talon(0);
+  frontRight=new Talon(1);
+  backLeft=new Talon(2);
+  backRight=new Talon(3);
+  drive=new RobotDrive(frontLeft, frontRight, backLeft, backRight);
 }
 void Drivetrain::InitDefaultCommand() {
+}
+void Drivetrain::DriveMecanum(float x, float y, float rotation){
+  drive->MecanumDrive_Cartesian(x, y, rotation);
+}
+void Drivetrain::DriveArcade(float x, float y){
+  drive->ArcadeDrive(x, y);
 }
