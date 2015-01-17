3dd19f8a5398ed88226e5b36b22267b95d8767b8
Austen Adler <stonewareslord@gmail.com>
2015-01-16 20:46:58 -0500

    Added elevator code

--- src/Subsystems/Elevator.cpp
+++ src/Subsystems/Elevator.cpp
@@ -1,7 +1,17 @@
 #include "Elevator.h"
 #include "../RobotMap.h"
-
-Elevator::Elevator() : Subsystem("Elevator") {
+Elevator::Elevator() : PIDSubsystem("Elevator", kP_real, kI_real, 0.0){
+  pot=new AnalogPotentiometer(0);
+  leftMotor=new Talon(1);
+  rightMotor=new Talon(0);
+  SetAbsoluteTolerance(0.004);
 }
 void Elevator::InitDefaultCommand() {
+}
+float Elevator::GetPotValue(){
+  return pot->Get();
+}
+void Elevator::Run(double power){
+  leftMotor->Set(power);
+  rightMotor->Set(power);
 }
--- src/Subsystems/Elevator.h
+++ src/Subsystems/Elevator.h
@@ -2,11 +2,17 @@
 #define ELEVATOR_H
 
 #include "WPILib.h"
-class Elevator: public Subsystem
+#include "Commands/PIDSubsystem.h"
+class Elevator: public PIDSubsystem
 {
   private:
+    AnalogPotentiometer *pot;
+    Talon *leftMotor, *rightMotor;
+    static constexpr double kP_real=4, kI_real=.0f, kP_simulation=18, kI_simulation=.2;
   public:
     Elevator();
     void InitDefaultCommand();
+    float GetPotValue();
+    void Run(double);
 };
 #endif
