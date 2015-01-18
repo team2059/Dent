c45ab0e9a2b58e0d01997f187dfc3ea496bb7582
Adam Long <macadam.long@gmail.com>
2015-01-18 09:54:36 +0000

    Fixed the axis locking

--- src/HHRobot.cpp
+++ src/HHRobot.cpp
@@ -9,27 +9,21 @@ HHRobot::HHRobot():
   }
 void HHRobot::Init(){
   printf("Initing\n");
+  AirCompressor->Start();
 }
 //Main function used to handle periodic tasks on the robot
 void HHRobot::Handler(){
   //Code to lock the axis when driving
   double x,y,z;
   if (DriveStick->GetJoystickButton(1)){
-    y = 0;
-    z = 0;
-  }else{
-    y = DriveStick->GetJoystickAxis("y");
-    z = DriveStick->GetJoystickAxis("z");
-  }
-  if (DriveStick->GetJoystickButton(2)){
-    x = 0;
-    z = 0;
-  }else{
     x = DriveStick->GetJoystickAxis("x");
-    z = DriveStick->GetJoystickAxis("z");
+  }else{
+    x = 0;
   }
-  //X axis, Y axis, Z axis, acceleration, threshold, gyro
-  RobotDrive->handler(x,y,z,0.5,1,0);
+  y = DriveStick->GetJoystickAxis("y");
+  z = DriveStick->GetJoystickAxis("z");
+  //X axis, Y axis, Z axis, sensitivity, mode threshold, gyro
+  RobotDrive->handler(x,y,z,0.95,DriveStick->GetThrottle(),0);
 
   //Disables the compressor if the battery voltage is detected to be less than 10 volts
   if (PowerDistPanel->GetVoltage()<=10){
