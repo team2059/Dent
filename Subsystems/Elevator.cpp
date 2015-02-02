0ff142c25b3cbc22010d2f2c10606399b6c42d36
Austen Adler <stonewareslord@gmail.com>
2015-02-02 18:53:02 -0500

    Updated to match robot specs

--- Subsystems/Elevator.cpp
+++ Subsystems/Elevator.cpp
@@ -1,9 +1,7 @@
 #include "Elevator.h"
 #include "../RobotMap.h"
 Elevator::Elevator()/* : PIDSubsystem("Elevator", kP_real, kI_real, 0.0)*/{
-  pot=new AnalogPotentiometer(0);
-  leftMotor=new CANTalon(1);
-  rightMotor=new CANTalon(0);
+  motor=new CANTalon(0);
   elevatorEncoder=new Encoder(0,1,false);
   offset=0;
   height=0;
@@ -11,12 +9,8 @@ Elevator::Elevator()/* : PIDSubsystem("Elevator", kP_real, kI_real, 0.0)*/{
 }
 void Elevator::InitDefaultCommand(){
 }
-float Elevator::GetPotValue(){
-  return pot->Get();
-}
 void Elevator::Run(double power){
-  leftMotor->Set(power);
-  rightMotor->Set(power);
+  motor->Set(power);
 }
 void Elevator::SetOffset(double ht){
   offset=ht;
