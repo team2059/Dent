04afbf4d21048a4c54a941d25179d6d15fad7e94
Austen Adler <stonewareslord@gmail.com>
2015-02-11 20:49:56 -0500

    Updated binElevator

--- Subsystems/BinElevator.cpp
+++ Subsystems/BinElevator.cpp
@@ -3,25 +3,19 @@
 BinElevator::BinElevator(){
   motor=new CANTalon(BINELEVATOR_CAN);
   elevatorEncoder=new Encoder(BINELEVATOR_ENCODERA,BINELEVATOR_ENCODERB,false);
-  offset=0;
-  height=0;
   elevatorBottom=new DigitalInput(BINELEVATOR_BOTTOM_DIO);
   elevatorTop=new DigitalInput(BINELEVATOR_TOP_DIO);
-  //SetAbsoluteTolerance(0.004);
 }
 void BinElevator::InitDefaultCommand(){
 }
 void BinElevator::Run(double power){
   motor->Set(power);
 }
-void BinElevator::SetOffset(double ht){
-  offset=ht;
-}
 void BinElevator::ResetEncoder(){
   elevatorEncoder->Reset();
 }
 double BinElevator::GetHeight(){
-  return elevatorEncoder->Get()+offset;
+  return elevatorEncoder->Get();
 }
 bool BinElevator::GetElevatorBottom(){
   return elevatorBottom->Get();
