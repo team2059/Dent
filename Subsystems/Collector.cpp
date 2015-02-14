7665f16a9ba5e40aa3daa7f9789edb1539f19d7f
Austen Adler <stonewareslord@gmail.com>
2015-02-14 17:12:58 -0500

    Added collectorMotorRamp

--- Subsystems/Collector.cpp
+++ Subsystems/Collector.cpp
@@ -4,6 +4,7 @@
 Collector::Collector() : Subsystem("Collector"){
   collectorMotorLeft=new CANTalon(COLLECTOR_LEFT_CAN);
   collectorMotorBottom=new CANTalon(COLLECTOR_BOTTOM_CAN);
+  collectorMotorRamp=new CANTalon(COLLECTOR_RAMP_CAN);
   collectorMotorRight=new CANTalon(COLLECTOR_RIGHT_CAN);
 }
 void Collector::InitDefaultCommand(){
@@ -11,6 +12,7 @@ void Collector::InitDefaultCommand(){
 void Collector::MoveRollers(double a){
   collectorMotorLeft->Set(a);
   collectorMotorBottom->Set(a);
+  collectorMotorRamp->Set(a);
   collectorMotorRight->Set(-a);
 }
 // vim: ts=2:sw=2:et
