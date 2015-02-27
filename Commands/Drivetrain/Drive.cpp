23f58f064474fa3a05034cf3285bf779f7fc401e
Austen Adler <stonewareslord@gmail.com>
2015-02-27 16:42:11 -0500

    Negated drive y value (untested)

--- Commands/Drivetrain/Drive.cpp
+++ Commands/Drivetrain/Drive.cpp
@@ -8,8 +8,8 @@ void Drive::Initialize(){
 void Drive::Execute(){
   double x, y, z;
   x = DentRobot::oi->GetLeftStick()->GetRawAxis(0);
+  y = -DentRobot::oi->GetLeftStick()->GetRawAxis(1);
   z = DentRobot::oi->GetLeftStick()->GetRawAxis(2);
-  y = DentRobot::oi->GetLeftStick()->GetRawAxis(1);
   //Code to lock the x axis when not holding button 1
   //if (DentRobot::oi->GetLeftStick()->GetRawButton(1)){
   //  x=0;
