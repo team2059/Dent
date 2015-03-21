76e282b413ed747caaae149b16ff73c42bf5e565
Adam Long <macadam.long@gmail.com>
2015-03-21 11:23:29 +0000

    Added independent speed control for collector motors

--- Commands/Drivetrain/Drive.cpp
+++ Commands/Drivetrain/Drive.cpp
@@ -22,7 +22,11 @@ void Drive::Execute(){
     x = -x;
     y = -y;
   }
-  DentRobot::drivetrain->DriveMecanum(x, y, z, 0.9);
+  if(DentRobot::oi->GetLeftStick()->GetRawButton(7)){
+    DentRobot::drivetrain->DriveMecanum(0.0, 0.0, 0.0, 0.9);
+  }else{
+    DentRobot::drivetrain->DriveMecanum(x, y, z, 0.9);
+  }
 }
 bool Drive::IsFinished(){
   return IsTimedOut();
