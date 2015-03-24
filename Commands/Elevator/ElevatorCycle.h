dd630e2574cf86643cdbd4faa6a583261a725c7f
Adam Long <macadam.long@gmail.com>
2015-03-24 18:42:11 +0000

    Added a main elevator cycle command group

---
+++ Commands/Elevator/ElevatorCycle.h
@@ -0,0 +1,23 @@
+#ifndef ELEVATORCYCLE_H
+
+#include "Commands/CommandGroup.h"
+#include "../../DentRobot.h"
+#include "WPILib.h"
+
+/**
+ * @brief Completes one cycle with the main elevator
+ *
+ * Completes a cycle by lowering the elevator to the bottom sensor,
+ * then raising the elevator the the top sensor.
+ * Timeouts are still in place in the event of a sensor failure.
+ */
+
+class ElevatorCycle: public CommandGroup{
+  public:
+    /**
+     * @brief Constructs Elevatorcycle
+     */
+    ElevatorCycle();
+};
+#endif
+// vim: ts=2:sw=2:et
